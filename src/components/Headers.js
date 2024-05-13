a8244436a1319badd52ed37eb408bcd1365447d1
Piyush0049 <piyushjoshi81204@gmail.com>
2024-05-13 18:38:47 +0530

    commit

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -36,8 +36,10 @@ function Headers() {
   }
 
   const { _id } = useSelector((state) => state.userdetails.user);
+  if(isAuthenticated){
   let selectedproducts = cartitems.filter((item) => item.user_id === _id);
   console.log(selectedproducts);
+  }
 
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
