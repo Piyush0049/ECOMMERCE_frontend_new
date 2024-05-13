fdfb8695bc5f8249f5807272d1dff1a871df0e18
Piyush0049 <piyushjoshi81204@gmail.com>
2024-05-13 18:25:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 import { Elements } from "@stripe/react-stripe-js";
