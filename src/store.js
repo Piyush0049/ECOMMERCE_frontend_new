c405f9576a73b6ab60505266c4bd73caf97d92be
Piyush0049 <piyushjoshi81204@gmail.com>
2024-05-16 16:01:12 +0530

    commit

--- src/store.js
+++ src/store.js
@@ -1,42 +1,40 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import {thunk} from 'redux-thunk'; // Import as named export
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { productreducer } from './reducers/productReducer';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import {thunk} from 'redux-thunk';
+import {composeWithDevTools} from "redux-devtools-extension"
+import {  productreducer } from './reducers/productReducer';
 import { alluserreducer, profilereducer, userreducer } from './reducers/usereducer';
 import { productDETAILreducer } from './reducers/productReducer';
 import { cartreducer } from './reducers/cartreducer';
 import { allordersreducer, orderreducer } from './reducers/orderreducer';
 
 const reducer = combineReducers({
-    products: productreducer,
-    productdetails: productDETAILreducer,
-    userdetails: userreducer,
-    updatedprofile: profilereducer,
-    cart: cartreducer,
-    myorders: orderreducer,
-    AllUsers: alluserreducer,
-    allorders: allordersreducer
+    products: productreducer ,
+    productdetails : productDETAILreducer,
+    userdetails : userreducer,
+    updatedprofile : profilereducer,
+    cart : cartreducer,
+    myorders : orderreducer,
+    AllUsers : alluserreducer,
+    allorders : allordersreducer
 });
 
 let initstate = {
-    cart: {
-        cartitems: localStorage.getItem("cartitem") !== "" ?
-            JSON.parse(localStorage.getItem("cartitem")) :
-            []
+    cart : {
+        cartitems : localStorage.getItem("cartitem")!== "" ?
+        JSON.parse(localStorage.getItem("cartitem")) : 
+        []
     },
-    myorders: {
-        orderdet: localStorage.getItem("myorder") ?
-            JSON.parse(localStorage.getItem("myorder")) :
-            []
+    myorders : {
+        orderdet : localStorage.getItem("myorder") ?
+        JSON.parse(localStorage.getItem("myorder")) : 
+        []
     }
 };
 const middleware = [thunk];
 const store = createStore(
     reducer,
     initstate,
-    composeWithDevTools(applyMiddleware(...middleware),
-),
-
+    composeWithDevTools(applyMiddleware(...middleware))
 );
 
-export default store;
+export default store
