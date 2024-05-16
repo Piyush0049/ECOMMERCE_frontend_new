27a8ed45b9155f8b7796abbc8677ed50a8d08e28
Piyush0049 <piyushjoshi81204@gmail.com>
2024-05-16 12:00:58 +0530

    commit

--- src/store.js
+++ src/store.js
@@ -34,7 +34,12 @@ const middleware = [thunk];
 const store = createStore(
     reducer,
     initstate,
-    composeWithDevTools(applyMiddleware(...middleware))
+    composeWithDevTools(applyMiddleware(...middleware),
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+    ? a => a
+    : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()),
+
 );
 
 export default store;
