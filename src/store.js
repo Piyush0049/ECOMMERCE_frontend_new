fc3e7b6d3cf99641903551103d2b33ac86256a34
Piyush0049 <piyushjoshi81204@gmail.com>
2024-05-16 16:30:49 +0530

    commit

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'; 
+import {thunk} from 'redux-thunk';
 import {composeWithDevTools} from "redux-devtools-extension"
 import {  productreducer } from './reducers/productReducer';
 import { alluserreducer, profilereducer, userreducer } from './reducers/usereducer';
