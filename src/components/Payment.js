2d8cd12ad1ee7234876d608a6fa0a7e514454d65
Piyush0049 <piyushjoshi81204@gmail.com>
2024-05-18 18:49:18 +0530

    commit

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import backimage from "./snapedit_1711040704089.jpeg";
 import { Link, useNavigate } from 'react-router-dom';
+import { useStripe, CardNumberElement, CardCvcElement, CardExpiryElement, useElements } from '@stripe/react-stripe-js';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { createorder } from './actions/orderactions';
-import { useStripe } from '@stripe/react-stripe-js';
 
 const Payment = () => {
   const [x, setx] = useState("");
@@ -15,35 +15,34 @@ useEffect(() => {
         setx(window.innerWidth);
     }
   }, []);
-  const stripe = useStripe(); 
-  const [cardDetails, setCardDetails] = useState({
-    cardNumber: '',
-    expiry: '',
-    cvc: '' 
-  });
+  
   const [paymentError, setPaymentError] = useState(null);
   const [isProcessing, setIsProcessing] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
   const totalprice = Number(localStorage.getItem("totalprice"));
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const totalmoney = totalprice;
-  const { cartitems } = useSelector((state) => state.cart);
+  const { cartitems } = useSelector((state) => state.cart)
   const shippingdet = JSON.parse(localStorage.getItem("shippingdetails"));
 
   const userAddress = {
     line1: shippingdet.userDetails.address,
     line2: shippingdet.userDetails.address,
-    city: shippingdet.selectedCity.label,
-    state: shippingdet.selectedState.value,
+    city: JSON.parse(localStorage.getItem("shippingdetails")).selectedCity.label,
+    state: JSON.parse(localStorage.getItem("shippingdetails")).selectedState.value,
     country: "US",
     postal_code: "125001",
   };
 
+
+
   const orderdis = {
     shippinginfo: {
       address: shippingdet.userDetails.address,
-      city: shippingdet.selectedCity.label,
-      state: shippingdet.selectedState.value,
+      city: JSON.parse(localStorage.getItem("shippingdetails")).selectedCity.label,
+      state: JSON.parse(localStorage.getItem("shippingdetails")).selectedState.value,
       country: "US",
       pincode: 125001,
       phoneno: shippingdet.userDetails.phone,
@@ -53,25 +52,17 @@ useEffect(() => {
       id: "",
       status: "succeeded"
     },
+
     itemsPrice: (totalprice * 100) / 118,
     taxPrice: (totalprice * 18) / 118,
     shippingPrice: 0,
     totalPrice: totalmoney,
     orderStatus: "pending"
-  };
+  }
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    console.log(cardDetails)
-    setCardDetails({
-      ...cardDetails,
-      [name]: value
-    });
-  };
 
   const handlePayment = async (e) => {
     e.preventDefault();
@@ -80,30 +71,22 @@ useEffect(() => {
 
       try {
         const config = {
-          headers: {
-            "Content-Type": "application/json"
-          }
+          "Content-Type": "application/json"
         };
         const { data } = await axios.post("https://snap-n-shop-fullmernstack-ecommerce.onrender.com/api/v1/payment/process", {
           description: "Description of the export transaction goes here",
           amount: Math.round(totalprice * 100),
-        }, config);
+        }, config, { withCredentials: true});
 
         const clientSecret = data.client_secret;
         const result = await stripe.confirmCardPayment(clientSecret, {
           payment_method: {
-            card: {
-              number: cardDetails.cardNumber,
-              exp_month: cardDetails.expiry.split('/')[0],
-              exp_year: cardDetails.expiry.split('/')[1],
-              cvc: cardDetails.cvc,
-            },
+            card: elements.getElement(CardNumberElement),
             billing_details: {
               name: shippingdet.userDetails.name,
-              email: shippingdet.userDetails.email,
+              email: JSON.parse(localStorage.getItem("shippingdetails")).userDetails.email,
               address: userAddress
-            },
-            payment_method: "pm_1PGwhhSHcH5kC6C6P8gjGcXk",
+            }
           }
         });
 
@@ -126,41 +109,7 @@ useEffect(() => {
         setIsProcessing(false);
       }
     }
-  };
 
-  const styles = {
-    container: {
-      minHeight: x >= 692 ? '1000px' : '3000px', 
-      minWidth: x >= 692 ? '1540px' : '1540px',
-      height: "auto",
-      width: "auto",
-      alignItems: 'center',
-      background: '#f0f0f0',
-      backgroundImage: `url(${backimage})`, 
-      backgroundSize: 'cover'
-    },
-    cardForm: {
-      width: '500px',
-      padding: '20px',
-      borderRadius: '10px',
-      boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
-      background: '#fff',
-    },
-    hr2: {
-      borderWidth: "2px",
-      opacity: 0.6,
-      width: "300px",
-    },
-    input: {
-      display: 'block',
-      width: '100%',
-      padding: '10px',
-      margin: '10px 0',
-      fontSize: '16px',
-      borderRadius: '5px',
-      border: '1px solid #ccc',
-      boxSizing: 'border-box'
-    }
   };
 
   return (
@@ -180,46 +129,60 @@ useEffect(() => {
               <b><hr style={{ width: "200px", backgroundColor: "black", position: "relative", bottom: "14px" }} /></b>
             </div>
           </div>
-          <form onSubmit={handlePayment}>
-            <div style={{ justifyContent: "center", position: "relative", top: "40px" }}>
-              <input
-                type="text"
-                name="cardNumber"
-                value={cardDetails.cardNumber}
-                placeholder="1234 1234 1234 1234"
-                onChange={handleInputChange}
-                style={styles.input}
-              />
-              <input
-                type="text"
-                name="expiry"
-                value={cardDetails.expiry}
-                placeholder="MM/YY"
-                onChange={handleInputChange}
-                style={styles.input}
-              />
-              <input
-                type="text"
-                name="cvc"
-                value={cardDetails.cvc}
-                placeholder="CVC"
-                onChange={handleInputChange}
-                style={styles.input}
-              />
-            </div>
-            {paymentError && <div style={{ color: 'red', textAlign: 'center', marginTop: '20px' }}>{paymentError}</div>}
-            <div style={{ display: "flex", justifyContent: "center", marginTop: "30px" }}>
-              <button type="submit" className="btn btn-warning" disabled={isProcessing}>
-                {isProcessing ? "Processing..." : `Pay ₹${totalprice}`}
-              </button>
-            </div>
-          </form>
+          <div style={{ justifyContent: "center", position: "relative", top: "40px" }}>
+            <CardNumberElement options={cardElementOptions} />
+            <CardExpiryElement options={cardElementOptions} />
+            <CardCvcElement options={cardElementOptions} />
+          </div>
+          {paymentError && <div style={{ color: 'red' }}>{paymentError}</div>}
+          <div style={{ display: "flex", justifyContent: "center", marginTop: "30px" }}>
+            <button onClick={handlePayment} type="button" className="btn btn-warning" style={{ padding: "10px", paddingInline: "40px", fontSize: x >= 692 ? "20px" : '55px', position: "relative", top: "80px" }} disabled={isProcessing}>
+              {isProcessing ? "Processing..." : `Pay ₹${totalprice}`}
+            </button>
+          </div>
+
         </div>
       </div>
     </div>
   );
 };
-
-
+const cardElementOptions = {
+  style: {
+    base: {
+      fontSize: x >= 692 ? "25px" : '45px',
+      color: 'green',
+      '::placeholder': {
+        color: 'gray',
+      },
+      textAlign: "center",
+    },
+    invalid: {
+      color: 'red',
+    },
+  },
+};
+const styles = {
+  container: {
+    minHeight: x >= 692 ? '1000px' : '3000px', // Adjusted height based on window width
+    minWidth: x >= 692 ? '1540px' : '1540px',
+    height: "auto",
+    width: "auto",
+    alignItems: 'center',
+    background: '#f0f0f0',
+    backgroundImage: `url(${backimage})`, backgroundSize: 'cover'
+  },
+  cardForm: {
+    width: '500px',
+    padding: '20px',
+    borderRadius: '10px',
+    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+    background: '#fff',
+  },
+  hr2: {
+    borderWidth: "2px",
+    opacity: 0.6,
+    width: "300px",
+  },
+};
 
 export default Payment;
