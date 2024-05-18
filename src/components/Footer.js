82a40cf658a379e8f245f81f425ddb3b226ff636
Piyush0049 <piyushjoshi81204@gmail.com>
2024-05-18 13:37:41 +0530

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { useState } from 'react';
+import { useEffect } from 'react';
 function Footer() {
   const [x, setx] = useState("");
 useEffect(() => {
